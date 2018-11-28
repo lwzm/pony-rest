26d04c85148dd7f0524c2b4f397cc95789b62588
q <lwzm@qq.com>
2018-11-28 13:04:58 +0800

    ExportHandler

--- pony_rest.py
+++ pony_rest.py
@@ -51,6 +51,72 @@ class BaseHandler(tornado.web.RequestHandler):
         return self._json
 
 
+class ExportHandler(BaseHandler):
+    def get(self):
+        from pony.orm import Json
+        formats_for_js = {
+            str: "string",
+            int: "number",
+            float: "float",
+            bool: "boolean",
+            datetime: "datetime",
+            date: "date",
+            Json: "json",
+        }
+        lst = []
+        pks = {}
+        try:
+            import yaml
+            with open("patch.yaml") as f:
+                patch = yaml.load(f)
+        except FileNotFoundError:
+            patch = {}
+        # 1
+        for table in BaseEntity.__subclasses__():
+            tableName = table.__name__.lower()
+            tablePatch = patch.get(tableName, {})
+            pk = None
+            fs = []
+            for column in table._attrs_:
+                columnName = column.column
+                if not columnName:
+                    continue
+                assert column.py_type, column
+                if column.is_pk:
+                    pk = columnName
+                    pks[table] = {
+                        "tableName": tableName,
+                        "columnName": columnName,
+                    }
+                py_type = column.py_type
+                type = formats_for_js.get(py_type, py_type)
+                if type == "string" and not column.args:
+                    type = "text"
+                o = {
+                    "columnName": columnName,
+                    "type": type,
+                }
+                o.update(tablePatch.pop(columnName, {}))
+                fs.append(o)
+
+            t = {
+                "tableName": tableName,
+                "primaryKey": pk,
+                "fs": fs,
+            }
+            t.update(tablePatch)
+            lst.append(t)
+        # 2
+        for i in lst:
+            for f in i["fs"]:
+                t = f["type"]
+                if not isinstance(t, str):
+                    assert issubclass(t, Entity), t
+                    f["foreignKey"] = pks[f.pop("type")]
+        # 3
+        self.write_json(lst)
+
+
 database = pony.orm.Database()
 BaseEntity = database.Entity
 
@@ -186,6 +252,7 @@ def make_app():
         magic_it(i)
         for i in BaseEntity.__subclasses__()
     ]
+    handlers.append(("/", ExportHandler))
     return tornado.web.Application(handlers)
 
 
